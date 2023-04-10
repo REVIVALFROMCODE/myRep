bd3d9cd78bdb19c046d1a0658202d712aef74975
REVIVALFROMCODE <56755895+REVIVALFROMCODE@users.noreply.github.com>
2023-04-11 00:25:58 +0800

    Create 二叉树递归遍历.go

---
+++ 二叉树递归遍历.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {//leetcode 0094,0144,0145
+    values:=make([]int,0,100)//[]int is beyond function,as global value
+    var help func(*TreeNode)
+    help=func(root *TreeNode){//if you make []int as parameter but no return is totally wrong.Nothing will change in slice.
+        if root==nil{//maybe you can pass &[]int as parameter.
+            return
+        }
+        values=append(values,root.Val)
+        help(root.Left)
+        help(root.Right)
+    }
+    help(root)
+    return values
+}
