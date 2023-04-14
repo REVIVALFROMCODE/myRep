65b3ca50529df9f2a4ca56751c63af885ee1dbb2
REVIVALFROMCODE <56755895+REVIVALFROMCODE@users.noreply.github.com>
2023-04-14 21:29:56 +0800

    Update 二叉树递归遍历.go

--- 二叉树递归遍历.go
+++ 二叉树递归遍历.go
@@ -9,8 +9,8 @@
 func preorderTraversal(root *TreeNode) []int {//leetcode 0144 preorderTraversal,0094 inorderTraversal,0145 postorderTraversal
     values:=make([]int,0,100)//[]int is beyond function,as global value
     var help func(*TreeNode)
-    help=func(root *TreeNode){//if you make []int as parameter but no return is totally wrong.Nothing will change in slice.
-        if root==nil{//maybe you can pass &[]int as parameter.
+    help=func(root *TreeNode){//if you make []int as parameter but no return is totally wrong.Nothing will append in slice.
+        if root==nil{//maybe you can pass &[]int as parameter.传slice可以更改原有数据,改的是底层数组,不能增加数据
             return
         }
         values=append(values,root.Val)
