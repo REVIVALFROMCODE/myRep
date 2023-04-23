c6a5a410051db52628aeffeea403dd69aca7c531
REVIVALFROMCODE <56755895+REVIVALFROMCODE@users.noreply.github.com>
2023-04-23 13:52:21 +0800

    Update 二叉树递归遍历.go

--- 二叉树递归遍历.go
+++ 二叉树递归遍历.go
@@ -5,12 +5,19 @@
  *     Left *TreeNode
  *     Right *TreeNode
  * }
+ *
+ *[]int is beyond function,as global value.Here is the trick , no need to pass the slice.
+ *If you make slice as parameter but no return is totally wrong.
+ *Nothing will append in slice.
+ *The reason why is you pass the struct by value,eventhough the base array pass by ptr,length and capacity which is integer type never update.
+ *The value is writen in memory in same base array but len and cap dont knonw that.
+ *leetcode 0144 preorderTraversal,0094 inorderTraversal,0145 postorderTraversal
  */
-func preorderTraversal(root *TreeNode) []int {//leetcode 0144 preorderTraversal,0094 inorderTraversal,0145 postorderTraversal
-    values:=make([]int,0,100)//[]int is beyond function,as global value
+func preorderTraversal(root *TreeNode) []int {
+    values:=make([]int,0,100)
     var help func(*TreeNode)
-    help=func(root *TreeNode){//if you make []int as parameter but no return is totally wrong.Nothing will append in slice.
-        if root==nil{//maybe you can pass &[]int as parameter.传slice可以更改原有数据,改的是底层数组,不能增加数据
+    help=func(root *TreeNode){
+        if root==nil{
             return
         }
         values=append(values,root.Val)
