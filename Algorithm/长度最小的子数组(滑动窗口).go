744e918c45f29cc327725474509b3c31cf86c0c9
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2023-08-10 13:39:16 +0800

    Update 长度最小的子数组(滑动窗口).go

--- Algorithm/长度最小的子数组(滑动窗口).go
+++ Algorithm/长度最小的子数组(滑动窗口).go
@@ -1,23 +1,30 @@
-func minSubArrayLen(target int, nums []int) int {//leetcode 0209
-    l,r,sum,length,minLen:=0,0,0,0,199999
-    min:=func(i,j int)int{
-        if i<j{
-            return i
-        }else{
-            return j
-        }
-    }
-    for r=0;r<len(nums);r++{//右侧边界先贪心扩大
-        sum+=nums[r]
-        for sum>=target{//符合条件后,收缩左侧边界
-            length=r-l+1
-            minLen=min(minLen,length)
-            sum-=nums[l]
-            l++
-        }
-    }
-    if minLen==199999{
-        return 0
-    }
-    return minLen
+func minSubArrayLen(target int, nums []int) int {
+var (
+		zero  = int(0)
+		l		  = zero
+		r		  = zero
+		sum		= zero
+		length	= zero
+		minLen 	= int(1e9)
+	)
+	min := func(i, j int) int {
+		if i < j {
+			return i
+		} else {
+			return j
+		}
+	}
+	for r = 0; r < len(nums); r++ {//plus right until sum get to target
+		sum += nums[r]
+		for sum >= target {//contract left , it may have better solution in window
+			length = r - l + 1 // key clause
+			sum -= nums[l]
+			minLen = min(minLen, length)
+			l++
+		}
+	}
+		if minLen == int(1e9) {
+		return 0
+	}
+	return minLen
 }
