72ca84baf97a25b36ada21d59e4d383aaeba7f9a
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2023-08-23 14:54:16 +0800

    Create 二叉树最大深度.go

---
+++ Algorithm/二叉树最大深度.go
@@ -0,0 +1,13 @@
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+} //自底向上返回的时候自加1,避免了自顶向下判断左右节点
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
