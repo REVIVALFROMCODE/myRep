a2bc66aa976ff3060cc251fc357a858ca84f85d5
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2023-08-23 17:24:28 +0800

    Update 二叉树直径.go

--- Algorithm/二叉树直径.go
+++ Algorithm/二叉树直径.go
@@ -9,11 +9,11 @@ func diameterOfBinaryTree(root *TreeNode) int {
 		if root.Left == nil && root.Right == nil {
 			return 1
 		}
-    //自底向上,最底左右皆空返回1.往上看一层,如左边一个且右边空返回1+0.
+    //自底向上,最底左右皆空返回1.往上看一层,如左边一个且右边空返回1+0.故得来以上两个递归出口.
 		left, right := helper(root.Left), helper(root.Right)
 		now := left + right
 		Max = max(now, Max)
-    //左路右路和是当前节点最大,往上一层,应是左右路中最大的一路+1做左或右分支.
+    //左路右路和是当前节点最大,记录max.往上一层,应是前左右路中最大的一路+1做左或右分支.
 		return 1 + max(left, right)
 	}
 	helper(root)
