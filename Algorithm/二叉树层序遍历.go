1ce27571ae0ff0b5cf50ccde5e4edd7411000d6b
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2023-08-24 17:19:09 +0800

    Update 二叉树层序遍历.go

--- Algorithm/二叉树层序遍历.go
+++ Algorithm/二叉树层序遍历.go
@@ -33,15 +33,15 @@ func levelOrder(root *TreeNode) [][]int {
 
 	for !q.empty() {
 		size := len(q.data) //初始状态
-		row := make([]int, 0, size)
-		for i := 0; i < size; i++ {
-			cur := q.pop()
-			row = append(row, cur.Val)
+		row := make([]int, 0, size)//满二叉树,每一层row有2^n个节点,实际情况是队列长度个节点.但队列还要不断装入子节点,故记录size.
+		for i := 0; i < size; i++ {//遍历每一层初始状态节点.
+			cur := q.pop()//2.出队
+			row = append(row, cur.Val)//3.记录
 
-			q.push(cur.Left)
+			q.push(cur.Left)//4.son进queue
 			q.push(cur.Right)
 		}
-		res = append(res, row)
+		res = append(res, row)//5.[]append进[][],直接操作[][]太复杂.
 	}
 	return res
 }
