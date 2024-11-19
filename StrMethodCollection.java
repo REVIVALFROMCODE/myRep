a8205bdf09660dcf436c61806ba79c18c28534de
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2024-11-19 07:58:53 +0000

    Update StrMethodCollection.java

--- StrMethodCollection.java
+++ StrMethodCollection.java
@@ -1,29 +1,33 @@
 package org.example;
+
 //delegation chain is Indexof(subStr)->startsWith(prefix)->substring(index)->charAt(index)
 public class StrMethodCollection {
 
-    public boolean startWith(String haystack, String needle, int start){
-        return startWith(subString(haystack,start),needle);
+    public boolean startWith(String haystack, String needle, int start) {
+        return startWith(subString(haystack, start), needle);
     }
-    public static boolean startWith(String haystack, String needle){
-        for(int j=0; j<needle.length(); j++){
-            if(charAt(haystack,j)!=charAt(needle,j)) return false;
+
+    public static boolean startWith(String haystack, String needle) {
+        for (int j = 0; j < needle.length(); j++) {
+            if (charAt(haystack, j) != charAt(needle, j)) return false;
         }
         return true;
     }
-    public static String subString(String s, int index){
-        if(s==null || index>s.length()-1) return null;
 
-        char[] result = new char[s.length()-index];
-        for(int i =index; i< s.length(); i++){
-            result[s.length()-i]=charAt(s,i);
+    public static String subString(String s, int index) {
+        if (s == null || index > s.length() - 1) return null;
+
+        char[] result = new char[s.length() - index];
+        for (int i = index; i < s.length(); i++) {
+            result[s.length() - i] = charAt(s, i);
         }
         return new String(result);//result.toString() return hash code.
     }
-    public static char charAt(String s, int index){
-        try{
+
+    public static char charAt(String s, int index) {
+        try {
             return s.toCharArray()[index];
-        }catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             throw e;
         }
 
