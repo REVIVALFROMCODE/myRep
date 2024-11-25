335d63ec11ebcc102b11a13c11e77befcd55eb90
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2024-11-25 23:59:51 +0800

    Create partition.java

---
+++ partition.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        //1.New two dummy and current
+        ListNode lessHead = new ListNode(), greatHead = new ListNode(), lessCur = lessHead, greatCur = greatHead;
+        
+        //2.Itearate LinkedList and seperate 
+        for(ListNode cur=head; cur!=null ; cur=cur.next){
+            if(cur.val<x){
+                lessCur.next=cur;
+                lessCur = lessCur.next;
+            }else{
+                greatCur.next= cur;
+                greatCur=greatCur.next;
+            }
+        }
+        
+        //3.Merge
+        lessCur.next=greatHead.next;
+        greatCur.next=null;//do not forget.
+
+        return lessHead.next;
+    }
+}
