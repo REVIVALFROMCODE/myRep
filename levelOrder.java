c1b7207fb40ed04a6dfcb327d1a06effe0b9810a
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2024-12-06 09:32:45 +0000

    Implement queue

--- levelOrder.java
+++ levelOrder.java
@@ -23,3 +23,71 @@ class Solution {
         return result;
     }
 }
+
+
+//Solution 2, implement queue manually, we don't have to change any code in solution as we implement same interface: add,poll,isEmpty,size.
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null) return res;
+        queue<TreeNode> q = new queue<>();
+
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int currentSize = q.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for(int i =0;i<currentSize;i++){
+                TreeNode currentNode = q.poll();
+                currentLevel.add(currentNode.val);
+                if(currentNode.left!=null) q.add(currentNode.left);
+                if(currentNode.right!=null) q.add(currentNode.right);
+            }
+            res.add(currentLevel);
+        }
+        return res;
+    }
+
+    private class queue<Item>{
+        private class Node{//Inner class do not need <Item>
+            Item val;
+            Node next;
+
+            Node(Item x){
+                val=x;
+            }
+        }
+        Node prior;
+        Node tail;
+        int N;
+        void add(Item x){
+            Node newNode = new Node(x);
+            if(isEmpty()){
+                tail = newNode;
+                prior=tail;
+            }else{
+                tail.next=newNode;
+                tail=tail.next;
+            }
+            N++;
+        }
+
+        Item poll(){
+            Item val = prior.val;
+            prior=prior.next;
+            N--;
+            if(isEmpty()) tail=prior;//=null
+            return val;
+        }
+
+        boolean isEmpty(){
+            return prior==null;
+            //or N==0
+        }
+
+        int size(){
+            return N;
+        }
+    }
+}
