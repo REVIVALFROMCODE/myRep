b490fff206310715bc96d809a8cedeb1a387f514
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2024-12-09 09:46:59 +0000

    Update canJump.java

--- canJump.java
+++ canJump.java
@@ -16,3 +16,30 @@ Input: nums = [2,3,1,1,4]
 Output: true
 Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 */
+
+
+
+//II
+/*
+Intuition :
+At any position ( i ), the maximum range we can reach is ( i + nums[i] ).
+Using a greedy strategy, we try to jump as far as possible within our current range before incrementing the jump count.
+Track the maximum range we can reach in the current jump window. When we exhaust this range, it means we must take another jump.
+*/
+class Solution {
+    public int jump(int[] nums) {
+        if (nums.length <= 1) return 0;
+        int jumps = 0;
+        int currentEnd = 0;
+        int furthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            furthest = Math.max(furthest, i + nums[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = furthest;
+                if (currentEnd >= nums.length - 1) break;
+            }
+        }
+        return jumps;
+    }
+}
