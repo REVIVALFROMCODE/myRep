f1a0efa93d48ec5cfee82cdf3f481275f3e4ff7a
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2025-01-03 04:50:28 +0000

    Update isSameTree.java

--- isSameTree.java
+++ isSameTree.java
@@ -16,9 +16,11 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         
-        if ((p == null && q != null) || (p != null && q == null)) return false;  //if one of p,q is null, but another one is not, false.
+        //if ((p == null && q != null) || (p != null && q == null)) return false;  //if one of p,q is null, but another one is not, false.
         
         if (p==null && q==null) return true;  //both are null, true;
+
+        if(p==null||q==null) return false;//if one of p,q is null, but another one is not, false.
         
         if(p.val!=q.val) return false;  //different val, false;
         
