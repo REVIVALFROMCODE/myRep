4084bba813d8b818b083b1e3459c09bb6c3f6cf4
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2025-01-15 03:20:52 +0000

    Update MaxConsecutiveOnes.java

--- slidingWindow/MaxConsecutiveOnes.java
+++ slidingWindow/MaxConsecutiveOnes.java
@@ -38,31 +38,22 @@ Bolded numbers were flipped from 0 to 1. The longest subarray is scoped.
 /*
 GOAL:Find consecutive 1, we have number of k chance to flip 0 to 1.
 ALGO:Increment R pointer, if encounter 0, increment zeroCount. When zero count reach K, we shrink pointer L (if previous entry L is 0, we decrement zeroCount).
+Why k=2, 1,1,1,0,0,0,1 works as expected?
 
 */
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int left = 0;
-        int right = 0;
-        int maxLen = 0;
-        int zerosCount = 0;
-
-        while (right < nums.length) {
-            if (nums[right] == 0) {
-                zerosCount++;
+        int left=0,right =0;
+        int count=0;
+        int res=-1;
+        for(right=0;right<nums.length;right++){
+            if(nums[right]==0) count++;
+            while(count>k){
+                if(nums[left++]==0) count--;
             }
-
-            while (zerosCount > k) {
-                if (nums[left] == 0) {
-                    zerosCount--;
-                }
-                left++;
-            }
-
-            maxLen = Math.max(maxLen, right - left + 1);
-            right++;
+            res=Math.max(res,right-left+1);
         }
-
-        return maxLen;
+        return res;
     }
 }
+
