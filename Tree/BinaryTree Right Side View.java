2d6b765916ebee5038512047c5b6cf2e90402099
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2025-01-17 10:13:32 +0000

    Update BinaryTree Right Side View.java

--- Tree/BinaryTree Right Side View.java
+++ Tree/BinaryTree Right Side View.java
@@ -38,3 +38,25 @@ class Solution {
         return rightView;
     }
 }
+
+
+//Solution2
+class Solution {
+    List<Integer> result;
+    public List<Integer> rightSideView(TreeNode root) {
+        this.result = new ArrayList<>();
+        dfs(root, 0);
+        return result;
+    }
+
+    void dfs(TreeNode root, int level) {
+        if(root == null) return;
+
+        if(result.size() == level) {
+            result.add(root.val);
+        }
+
+        dfs(root.right, level+1);
+        dfs(root.left, level+1);
+    }
+}
