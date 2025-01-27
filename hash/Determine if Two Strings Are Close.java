4a5f3dc906681413d182c23373726281178f577a
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2025-01-27 03:12:34 +0000

    Create Determine if Two Strings Are Close.java

---
+++ hash/Determine if Two Strings Are Close.java
@@ -0,0 +1,45 @@
+/*
+Input: word1 = "cabbba", word2 = "abbccc"
+Output: true
+Explanation: You can attain word2 from word1 in 3 operations.
+Apply Operation 1: "cabbba" -> "caabbb"
+Apply Operation 2: "caabbb" -> "baaccc"
+Apply Operation 2: "baaccc" -> "abbccc"
+
+One shocking information is: 
+a 2  a 1
+b 3  b 2
+c 1  c 3
+Sort frequency: 1,2,3
+*/
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (char ch : word1.toCharArray()) {
+            freq1[ch - 'a']++;
+        }
+
+        for (char ch : word2.toCharArray()) {
+            freq2[ch - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if(freq1[i] != 0 || freq2[i] != 0){
+                if(freq1[i] == 0 || freq2[i] == 0) return false;
+            }
+        }
+
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
