cec916560923febed85fc2cc21aa995ddc8332a8
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2025-02-03 05:31:36 +0000

    Update Roman to Integer.java

--- Roman to Integer.java
+++ Roman to Integer.java
@@ -40,32 +40,34 @@ public class Solution {
     }
 }
 
-//Solution 2 
-class Solution {
-    public int romanToInt(String s) {
-        int sum = 0;
-        int previousRomanValue = 0;
-        for(int i = s.length()-1; i >= 0; i--){
-            int rv = romanValue(s.charAt(i));
-            if(rv < previousRomanValue) {
-                sum-=rv;
-                continue;
-            }
-            sum+=rv;
-            previousRomanValue = rv;
-        }
-        return sum;
-    }
-    private int romanValue(char c) {
-        switch(c) {
-            case 'I' : return 1;
-            case 'V' : return 5;
-            case 'X' : return 10;
-            case 'L' : return 50;
-            case 'C' : return 100;
-            case 'D' : return 500;
-            case 'M' : return 1000;
-            default : return 0;
-        }
-    }
+//Solution 2 backward order
+class Solution{
+	private int getRoman(char c){
+		switch(c){
+		case 'I':return 1;
+		case 'V':return 5;
+		case 'X':return 10;
+		case 'L':return 50;
+		case 'C':return 100;
+		case 'D':return 500;
+		case 'M':return 1000;
+		default: return 0;
+		}
+	}
+
+	public int romanToInt(String s){
+		int sum=0;
+		int previous=0;
+
+		for(int i=s.length()-1; i>=0; i--){
+			int rv=getRoman(s.charAt(i));
+			if(rv<previous){
+				sum-=rv;
+			}else{
+				sum+=rv;
+				previous=rv;
+			}
+		}
+		return sum;
+	}
 }
