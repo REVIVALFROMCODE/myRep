1c9866ded9724c87d04bc9e3a3bff3040bb73b24
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2025-02-03 05:03:36 +0000

    Update Integer to Roman.java

--- Integer to Roman.java
+++ Integer to Roman.java
@@ -18,6 +18,21 @@ If the value starts with 4 or 9 use the subtractive form representing one symbol
 
 Only powers of 10 (I, X, C, M) can be appended consecutively at most 3 times to represent multiples of 10. 
 You cannot append 5 (V), 50 (L), or 500 (D) multiple times. If you need to append a symbol 4 times use the subtractive form.
+
+Input: num = 3749
+
+Output: "MMMDCCXLIX"
+
+Explanation:
+
+3000 = MMM as 1000 (M) + 1000 (M) + 1000 (M)
+ 700 = DCC as 500 (D) + 100 (C) + 100 (C)
+  40 = XL as 10 (X) less of 50 (L)
+   9 = IX as 1 (I) less of 10 (X)
+Note: 49 is not 1 (I) less of 50 (L) because the conversion is based on decimal places
+
+Solution: 3749 = 3*1000 + 500 + 2*100 + 40 + 9
+                = MMM + D + CC + XL + IX
 */
 
 public class Solution {
