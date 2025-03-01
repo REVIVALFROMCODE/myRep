58edd73b2ebe14fcedb587d05afb1ec2313b0d82
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2025-03-01 22:48:25 +0800

    Update user.sql

--- DB/user.sql
+++ DB/user.sql
@@ -5,7 +5,7 @@ CREATE TABLE `user` (
   `salt` VARCHAR(64) NOT NULL,
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `phone` VARCHAR(20) UNIQUE,
-  `sex` ENUM('male', 'female') NOT NULL
+  `sex` ENUM('male', 'female') NOT NULL,
   `vip_level` TINYINT UNSIGNED DEFAULT 0 NOT NULL,
   `vip_expiry_date` DATETIME DEFAULT NULL,
   `free_vip_uses` INT DEFAULT 0 NOT NULL,
