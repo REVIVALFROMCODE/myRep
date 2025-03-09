4ace928a61b9e04adcc80fd2d0f8a7b9dd067993
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2025-03-09 14:53:25 +0800

    Update user.sql

--- DB/user.sql
+++ DB/user.sql
@@ -1,11 +1,10 @@
 CREATE TABLE `user` (
   `user_id` INT PRIMARY KEY,
   `user_name` VARCHAR(50) NOT NULL UNIQUE,
-  `password_hash` VARCHAR(255) NOT NULL,
-  `salt` VARCHAR(64) NOT NULL,
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `phone` VARCHAR(20) UNIQUE,
   `sex` ENUM('male', 'female') NOT NULL,
+  `birthday` DATE NOT NULL,
   `vip_level` TINYINT UNSIGNED DEFAULT 0 NOT NULL,
   `vip_expiry_date` DATETIME DEFAULT NULL,
   `free_vip_uses` INT DEFAULT 0 NOT NULL,
@@ -19,4 +18,16 @@ CREATE TABLE `vip_levels`(
   `name` VARCHAR(50) NOT NULL,
   `benefits` TEXT,
   `price` DECIMAL(10,2)
+);
+
+CREATE TABLE `user_profiles`(
+  `user_id` INT PRIMARY KEY,
+  `name` VARCHAR(50) NOT NULL,
+  `sex` ENUM('male', 'female') NOT NULL,
+  `birthday` DATE NOT NULL,
+);
+
+CREATE TABLE `user_images` (
+  `user_id` INT PRIMARY KEY,
+  `image` BLOB
 );
