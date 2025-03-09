89df36a2d09ab4dbbca55b1ea715dd7dd0c2c3f5
zhangHD <56755895+REVIVALFROMCODE@users.noreply.github.com>
2025-03-09 14:55:14 +0800

    Update BinaryHeap.java

--- PQ/BinaryHeap.java
+++ PQ/BinaryHeap.java
@@ -1,27 +1,62 @@
 package org.PQ;
+
 /*
-Based on complete binary tree
+Based on complete binary tree,level order traversal of tree is sorted order
  */
-public class BinaryHeap <Key extends Comparable<Key>> extends MaxPQ{
-    private Key[] pq;
+public class BinaryHeap<Key extends Comparable<Key>> extends MaxPQ<Key> {
+    private Key[] pq;//Immutable keys
     private int N;
 
-    private void swim(int k){
-
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
     }
 
-    private void sink(int k){
-        while (less(k,2*k)) {
-            int j = 2*k;
-            if(less(2*k,2*k+1)) j++;
-            exch(k,j);
+    private void exch(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
+    }
+    //heap-ordered parents no smaller than children's keys
+    //pq[k/2] >= pq[k] >= pq[2*k] and pq[2*k+1]
+
+    /*
+    bottom-up reheapify
+    When the priority of some node is increased (or a new node is added at the
+    bottom of a heap), we have to travel up the heap to restore the heap order.
+    */
+    private void swim(int k) {
+        // compare with parent k/2, since we represent the heap with array, the
+        // exchange is convincing.
+        while (k > 1 && less(k / 2, k)) {//less(k/2,k) violate heap-order
+            exch(k / 2, k);
+            k = k / 2;
         }
     }
 
-    private boolean less(int i, int j){
-        return pq[i].compareTo(pq[j]) < 0;
+    public void insert(Key k) {
+        pq[++N] = k;
+        swim(N);
     }
-    private void exch(int i, int j){
-        
+
+    private void sink(int k) {
+        // compare to son 2k
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(j, j + 1))
+                j++;
+            if (!less(k, j))
+                break;
+            exch(k, j);
+            k = j;
+        }
     }
+
+    public Key delMax() {
+        Key max = pq[1];//return pq[1]
+        exch(1, N--);//exchange with the last one
+        sink(1);//re-order it(was last one)
+        pq[N + 1] = null;//remove last one(was first one)
+        return max;
+    }
+//1
 }
