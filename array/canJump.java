23f7d2b4da895df621af63091d779e346e59a864
RevivalFromCode <781688309@qq.com>
2025-03-25 22:11:19 +0800

    commit

--- array/canJump.java
+++ array/canJump.java
@@ -28,21 +28,31 @@ Track the maximum range we can reach in the current jump window. When we exhaust
 */
 class Solution {
     public int jump(int[] nums) {
-        if (nums.length <= 1) return 0;
+        if (nums.length == 1) return 0;
         int jumps = 0;
         int currentEnd = 0;
         int furthest = 0;
+        if(furthest >= nums.length-1) return ++jumps;
         for (int i = 0; i < nums.length - 1; i++) {
             furthest = Math.max(furthest, i + nums[i]);
             if (i == currentEnd) {
                 jumps++;
                 currentEnd = furthest;
-                if (currentEnd >= nums.length - 1) break;
             }
         }
         return jumps;
     }
 }
+/*
+        //precondition: fur <= nums.length-1
+
+        //preservation: i+nums[i] = fur<= nums.length-1
+
+        //postcondition: fur=nums.length-1=cur, i: 0:cur
+
+        //iterate i, update cur, calculate fur;
+ */
+
 /**
  * currentEnd represents the farthest point that can be reached using the current number of jumps. It defines the range of indices you can explore before needing another jump.
 
